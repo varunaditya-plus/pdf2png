2b22e1bd164486a766951586a45aa59b4bd68c40
varun <varunaditya.2023@gmail.com>
2025-01-20 21:42:01 +0100

    Updated Shell Cmd

--- pdf2png.sh
+++ pdf2png.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-python3 app.py
+
+set -e
+
+VENV_DIR="$(dirname "$0")/.venv"
+if [ ! -d "$VENV_DIR" ]; then
+    python3 -m venv "$VENV_DIR"
+fi
+
+source "$VENV_DIR/bin/activate"
+pip install -r requirements.txt
+
+python3 app.py "$@"
+deactivate
